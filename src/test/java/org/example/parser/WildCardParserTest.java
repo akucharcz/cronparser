61a11779423eeb927098ec0ba671353aa33a7261
Andre <andrzejkucharczyk96@gmail.com>
2023-03-26 14:06:51 +0200

    remove idea files

--- src/test/java/org/example/parser/WildCardParserTest.java
+++ src/test/java/org/example/parser/WildCardParserTest.java
@@ -2,6 +2,7 @@ package org.example.parser;
 
 import org.example.model.FieldParsingType;
 import org.example.model.FieldTimeType;
+import org.example.model.ParsedCronField;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -17,15 +18,12 @@ class WildCardParserTest {
 
     private static Object[][] fieldTimeTypeAndExpectedRange() {
         return new Object[][]{
-                {FieldTimeType.MINUTE, List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25
-                        , 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
-                        49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59)},
-                {FieldTimeType.HOUR, List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-                        20, 21, 22, 23)},
-                {FieldTimeType.DAY_OF_THE_MONTH, List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
-                        24, 25, 26, 27, 28, 29, 30, 31)},
-                {FieldTimeType.MONTH, List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)},
-                {FieldTimeType.DAY_OF_THE_WEEK, List.of(0, 1, 2, 3, 4, 5, 6)}
+                {FieldTimeType.MINUTE, List.of("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15",
+                        "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59")},
+                {FieldTimeType.HOUR, List.of("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23")},
+                {FieldTimeType.DAY_OF_THE_MONTH, List.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31")},
+                {FieldTimeType.MONTH, List.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")},
+                {FieldTimeType.DAY_OF_THE_WEEK, List.of("0", "1", "2", "3", "4", "5", "6")}
         };
     }
 
@@ -53,10 +51,11 @@ class WildCardParserTest {
     @MethodSource("fieldTimeTypeAndExpectedRange")
     public void shouldReturnProperRangeForWildCard(Object fieldTimeType, Object values) {
         //given
+        var expectedCron = new ParsedCronField((FieldTimeType) fieldTimeType, (List<String>) values);
         //when
         var result = wildCardParser.parse("", (FieldTimeType) fieldTimeType);
         //then
-        assertEquals(result, values);
+        assertEquals(result, expectedCron);
 
     }
 }
